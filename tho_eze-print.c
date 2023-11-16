cfd5ff3dd1d4527978ee7f90bcb989d93c55da72
coxxy666 <ezeiyokecosmas@gmail.com>
2023-11-15 20:05:04 -0800

    print function

---
+++ tho_eze-print.c
@@ -0,0 +1,13 @@
+/**
+ *_printf - Prints a string to the specified output.
+ *@string: String to be printed.
+ *@x: Output to print.
+ *Return: void
+ */
+void _printf(char *string, int x)
+{
+int i = 0;
+
+for (; string[i] != '\0'; i++)
+write(x, &string[i], 1);
+}
