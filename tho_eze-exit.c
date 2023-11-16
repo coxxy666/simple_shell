4f5843224af918c5dff92a4d594f299456b6defd
coxxy666 <ezeiyokecosmas@gmail.com>
2023-11-15 20:03:45 -0800

    exit function

---
+++ tho_eze-exit.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _exitf - Exits the shell.
+ * @cm_d: The command entered.
+ * Description: No arg exits with
+ * current status arg exits with specified status.
+ * Return: void.
+ */
+void _exitf(char **cm_d)
+{
+int cmd_number = 0;
+int arg;
+
+for (; cm_d[cmd_number] != NULL; cmd_number++)
+;
+if (cmd_number == 1)
+{
+free(cm_d);
+free(line);
+free(cm_ds);
+exit(status);
+}
+else if (cmd_number == 2)
+{
+arg = _atoi(cm_d[1]);
+if (arg == -1)
+{
+_printf(shell_name, STDERR_FILENO);
+_printf(": 1: exit: Illegal number: ", STDERR_FILENO);
+_printf(cm_d[1], STDERR_FILENO);
+_printf("\n", STDERR_FILENO);
+status = 2;
+}
+else
+{
+free(line);
+free(cm_d);
+free(cm_ds);
+exit(arg);
+}
+}
+else
+_printf("$: exit doesn't take more than one argument\n", STDERR_FILENO);
+}
--- tho_eze-string.c
+++ tho_eze-string.c
@@ -27,19 +27,6 @@ x[delim_num] = NULL;
 return (x);
 }
 
-/**
- *_printf - Prints a string to the specified output.
- *@string: String to be printed.
- *@x: Output to print.
- *Return: void
- */
-void _printf(char *string, int x)
-{
-int i = 0;
-
-for (; string[i] != '\0'; i++)
-write(x, &string[i], 1);
-}
 
 /**
  *rem_newline - Removes newline characters from a string.
